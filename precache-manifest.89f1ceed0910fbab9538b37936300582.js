155d4cacf782b1d5764807ca5f4ec62b815d0252
edbertrhema <edbertrhema>
2019-11-21 09:33:58 +0700

    pesan kalian

---
+++ precache-manifest.89f1ceed0910fbab9538b37936300582.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6a908dafad2fb6a67c",
+    "url": "/css/about.d443fafb.css"
+  },
+  {
+    "revision": "04493d2f802d9f08f6ad",
+    "url": "/css/chunk-vendors.c893da24.css"
+  },
+  {
+    "revision": "91df4e2bac309b062a12e430555c672b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef6a908dafad2fb6a67c",
+    "url": "/js/about.001f9715.js"
+  },
+  {
+    "revision": "90165deb8e8fddd33b6b",
+    "url": "/js/app.68f15d27.js"
+  },
+  {
+    "revision": "04493d2f802d9f08f6ad",
+    "url": "/js/chunk-vendors.6934e8e0.js"
+  },
+  {
+    "revision": "fc9ed2078c725be9ff6b444b9ba1693e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
